5e06b467b51322c75914fc130b45a73bceb148e8
Aryan <aryanyadav0521@outlook.com>
2023-05-27 13:18:05 +0530

    Added logical.java

---
+++ lj_logical.java
@@ -0,0 +1,27 @@
+package com.learningJava;
+
+public class lj_logical {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+
+//        For AND
+//        if (a && b){
+//            System.out.println("Y");
+//        }
+//        else {
+//            System.out.println("N");
+//        }
+
+//        For OR
+//        if (a || b){
+//            System.out.println("Y");
+//        }
+//        else {
+//            System.out.println("N");
+//        }
+//        For NOT
+//        System.out.println(!a);
+//        System.out.println(!b);
+    }
+}
