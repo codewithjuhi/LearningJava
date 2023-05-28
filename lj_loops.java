d357609f108f4d2ffbb3124a08d0071c34314ed5
Aryan <aryanyadav0521@outlook.com>
2023-05-28 21:10:12 +0530

    Added loops.java

---
+++ lj_loops.java
@@ -0,0 +1,15 @@
+package com.learningJava;
+
+public class lj_loops {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+
+        int i = 1;
+        while (i<4){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
