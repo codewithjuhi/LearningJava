1a509c13aaa7d961d7929d9cee55fc2e8dcfc8ed
Aryan <aryanyadav0521@outlook.com>
2023-05-28 21:54:01 +0530

    Added doWhileLoop.java

---
+++ lj_doWhileLoop.java
@@ -0,0 +1,19 @@
+package com.learningJava;
+
+public class lj_doWhileLoop {
+    public static void main(String[] args) {
+/*
+        int a = 0;
+        while (a<5){
+            System.out.println(a);
+            a++;
+        }
+*/
+        /* DO-WHILE LOOP */
+        int b = 0;
+        do {
+            System.out.println(b);
+            b++;
+        }while (b<5);
+    }
+}
