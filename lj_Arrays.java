16cff058e5e6a5d076bb9e4911f938f795cff0ea
Aryan <aryanyadav0521@outlook.com>
2023-06-15 18:18:56 +0530

    Added Arrays.java

---
+++ lj_Arrays.java
@@ -0,0 +1,27 @@
+package com.learningJava;
+
+public class lj_Arrays {
+    public static void main(String[] args) {
+//        There are 3 main ways to create an array in java.
+
+//        1. Declaration and then memory allocation.
+//        int[] marks = new int[5];
+
+
+//       2. Declaration and then memory allocation.
+//        int[] marks;
+//        marks= new int[5];
+//        Initialization
+//        marks[0] = 100;
+//        marks[1] = 60;
+//        marks[2] = 70;
+//        marks[3] = 90;
+//        marks[4] = 86;
+
+//        3. Declaration, memory allocation and initialization all together.
+//        int[] marks = {100, 60, 70, 90, 86};
+
+//        Marks[5] =96; - Throws an error
+//        System.out.println(marks[4]);
+    }
+}
